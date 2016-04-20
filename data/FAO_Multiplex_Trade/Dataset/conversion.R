2370f532bcf555a76776302176922e84c3472922
Vincent Labatut <vince.labatut@gmail.com>
2016-04-20 16:13:37 +0200

    completed certain datasets

--- data/FAO_Multiplex_Trade/Dataset/conversion.R
+++ data/FAO_Multiplex_Trade/Dataset/conversion.R
@@ -20,23 +20,25 @@ for(layer in 1:layer.nbr)
 	g$name <- layer.list[layer,2]
 	V(g)$name <- node.list[,"nodeLabel"]
 	idx <- which(edge.list[,1]==layer)
-	flattened <- c(t(edge.list[idx,2:3]))
-	g <- add.edges(graph=g, edges=flattened, attr=list(weight=edge.list[idx,4]))
-#	g <- simplify(graph=g, remove.multiple=TRUE)
-	FAO[[layer]] <- g
-	
-	# record for muxviz
-	res.folder <- paste(folder,"MuxViz/",sep="")
-	dir.create(res.folder,showWarnings=FALSE,recursive=TRUE,)
-	layer.name <- sub(" ", "", layer.list[layer,2])
-	out.file <- paste(res.folder,layer.name,".edgelist",sep="")
-	el <- cbind(get.edgelist(g,names=FALSE)-1,E(g)$weight)
-	write.table(el,out.file,row.names=FALSE,col.names=FALSE)
-	node.names <- cbind(0:(vcount(g)-1),V(g)$name)
-	colnames(node.names) <- c("nodeID","nodeLabel")
-	out.file <- paste(res.folder,layer.name,".nodes",sep="")
-	write.table(node.names,out.file,row.names=FALSE,quote=FALSE)
-	cat("D:\\\\FAO_Multiplex_Trade\\\\MuxViz\\\\",layer.name,".edgelist;",layer.name,";D:\\\\FAO_Multiplex_Trade\\\\MuxViz\\\\",layer.name,".nodes\n",sep="")
+	if(length(idx)>1)
+	{	flattened <- c(t(edge.list[idx,2:3]))
+		g <- add.edges(graph=g, edges=flattened, attr=list(weight=edge.list[idx,4]))
+#		g <- simplify(graph=g, remove.multiple=TRUE)
+		FAO[[layer]] <- g
+		
+		# record for muxviz
+		res.folder <- paste(folder,"MuxViz/",sep="")
+		dir.create(res.folder,showWarnings=FALSE,recursive=TRUE,)
+		layer.name <- sub(" ", "", layer.list[layer,2])
+		out.file <- paste(res.folder,layer.name,".edgelist",sep="")
+		el <- cbind(get.edgelist(g,names=FALSE)-1,E(g)$weight)
+		write.table(el,out.file,row.names=FALSE,col.names=FALSE)
+		node.names <- cbind(0:(vcount(g)-1),V(g)$name)
+		colnames(node.names) <- c("nodeID","nodeLabel")
+		out.file <- paste(res.folder,layer.name,".nodes",sep="")
+		write.table(node.names,out.file,row.names=FALSE,quote=FALSE)
+		cat("D:\\\\FAO_Multiplex_Trade\\\\MuxViz\\\\",layer.name,".edgelist;",layer.name,";D:\\\\FAO_Multiplex_Trade\\\\MuxViz\\\\",layer.name,".nodes\n",sep="")
+	}
 }
 
 # record as R object
